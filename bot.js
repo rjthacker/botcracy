7a219fa2f354e0f30d801ee6a5086d8ad7b56d11
Ryan Thacker <ryanjthacker@gmail.com>
2020-12-03 02:34:03 -0500

    Nuke and help commands implemented

--- bot.js
+++ bot.js
@@ -9,9 +9,24 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', (message) => {
+  let targetMember = message.member.user;
+  if (message.content === '!help' || message.content === '!commands') {
+    message.channel.send(
+      `Hello ${targetMember}! This bot is currently being developed. Features will be added in the future, however for more info or if you wish to contribute, please message <@248030367666274304>.`
+    );
+  }
+});
+
 client.on('message', (msg) => {
-  if (msg.content === '!ping') {
-    msg.reply('Pong!');
+  if (msg.content === '!nuke') {
+    (async () => {
+      let fetched;
+      do {
+        fetched = await msg.channel.messages.fetch({ limit: 100 });
+        msg.channel.bulkDelete(fetched);
+      } while (fetched.size >= 2);
+    })();
   }
 });
 
