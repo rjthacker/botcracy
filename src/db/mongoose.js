22a51a38dfbb6f86bebdc33049ab5fcf8df6d7c7
Ryan Thacker <37236891+rjthacker@users.noreply.github.com>
2022-11-30 23:40:56 -0500

    mongoose.js updated to ES modules

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+import { config } from "dotenv";
+import mongoose from "mongoose";
+
+config({ path: `.env.${process.env.NODE_ENV}` });
 
 mongoose.connect(process.env.CONNECTION_URL, {
   useNewUrlParser: true,
