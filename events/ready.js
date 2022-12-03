1afac9fb9634c2f90e71e268868385e67dd46c27
Ryan Thacker <37236891+rjthacker@users.noreply.github.com>
2022-12-03 13:46:46 -0500

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,15 @@
+const { ActivityType } = require("discord.js");
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    client.user.setPresence({
+      activities: [{ name: `Being upgraded!`, type: ActivityType.Playing }],
+      status: "online",
+    });
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`I am in ${client.guilds.cache.size} servers`);
+  },
+};
