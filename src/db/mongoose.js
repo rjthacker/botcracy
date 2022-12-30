106c126bc4660714333ed495075b09e2dcaa86c4
Ryan Thacker <37236891+rjthacker@users.noreply.github.com>
2022-12-29 22:42:55 -0500

    strictQuery true default depreciation fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.CONNECTION_URL, {
   useNewUrlParser: true,
 });
