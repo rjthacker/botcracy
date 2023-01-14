a6a1e8a46dc9aa0e5f8a42cd77d0faac6fdbea80
Ryan Thacker <37236891+rjthacker@users.noreply.github.com>
2023-01-13 23:23:49 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,9 @@ module.exports = {
     .setName("help")
     .setDescription("Provides info about Botcracy"),
   async execute(interaction) {
-    await interaction.reply(
-      "Hey there! Botcracy is being rebuilt from scratch for the new Discord API and Discord.js Version 14. This is an example of the new slash commands, however it will take some time to rebuild all the old commands with the new slash api logic, however, they will be even better when completed! For updates and info, join the Botcracy discord server https://discord.gg/5XFCa65S5F"
-    );
+    await interaction.reply({
+      content: `Hey there!\n\nBotcracy is being rebuilt from scratch for the new Discord API and Discord.js Version 14. This is an example of the new slash commands, however it will take some time to rebuild all the old commands with the new slash api logic.\n\nFor updates and info, join the Botcracy discord server https://discord.gg/5XFCa65S5F`,
+      ephemeral: true,
+    });
   },
 };
